0c5f28d887c206863779a36c98612714bf75b189
desimetallica <ddesi89@gmail.com>
2020-04-27 12:00:20 +0200

    state and getters

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,51 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: "abc123", name: "Adam Jahr" },
+    categories: [
+      "sustainability",
+      "nature",
+      "animal welfare",
+      "housing",
+      "education",
+      "food",
+      "community"
+    ],
+    events: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+      { id: 3, text: "...", done: true },
+      { id: 4, text: "...", done: false }
+    ],
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+      { id: 3, text: "...", done: true },
+      { id: 4, text: "...", done: false }
+    ]
+  },
+  getters: {
+    // arrow function simplify pass state => return length
+    getCatLength: state => {
+      return state.categories.length;
+    },
+    //arrow function with more params I pass state and id
+    getEventById: state => id => {
+      // find return the event that has event id equal to id
+      return state.events.find(event => event.id === id);
+    },
+    getDoneTodos: state => {
+      return state.todos.filter(todo => todo.done);
+    },
+    getActiveTodos(state, getters) {
+      return getters.getTodosCount - getters.getDoneTodos;
+      // or return state.todos.filter(todo => !todo.done).length;
+    },
+    getTodosCount: state => {
+      return state.todos.length;
+    }
+  },
   mutations: {},
   actions: {},
   modules: {}
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <h1>Event Create</h1>
+    <h1>Event Create, {{ user.name }}</h1>
+    <ul>
+      <p>This event was created by {{ user.name }}</p>
+      <p>There are {{ getLength }} categories</p>
+      <li v-for="category in categories" :key="category.id">
+        {{ category }}
+      </li>
+      <p>{{ getEvent }}</p>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    getEvent() {
+      return this.$store.getters.getEventById(2);
+    },
+    getLength() {
+      return this.$store.getters.getCatLength;
+    },
+    ...mapState(["user", "categories"])
+  }
+};
 </script>
 
 <style></style>
